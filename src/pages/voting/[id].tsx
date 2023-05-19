6e09d1add48c16519ddd22d1f4f6463b28d4e791
Sergey Chernov <chernovsi@m2.ru>
2023-05-19 18:08:44 +0300

    add voting page

---
+++ src/pages/voting/[id].tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { useState } from 'react';
+
+import { voteServiceApi } from '../../services/vote.service';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export async function getServerSideProps({ params }: any) {
+    return {
+        props: {
+            taskId: params.id
+        },
+    };
+}
+
+
+export default function CreateTask({ taskId }: { taskId: string }) {
+    
+    const [score, setScore] = useState(0);
+    const handleScoreChange = (event: any) => {
+        setScore(event.target.value);
+    }
+
+    const vote = async (event: any) => {
+        event.preventDefault();
+        voteServiceApi.createVote({
+            username: '',
+            taskId,
+            score
+        })
+
+    }
+
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        >
+            <div>
+                <Image
+                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+                    src="/logo-large-gold.png"
+                    alt="Alice M2 Logo"
+                    width={232}
+                    height={167}
+                    priority
+                />
+            </div>
+            <label htmlFor="name">{`Проголосуйте за задачу ${taskId}:`}</label>
+                <input type="text" id="score" name="score" onChange={handleScoreChange} />
+                <button onClick={vote}>Послать</button>
+            
+        </main>
+    );
+}

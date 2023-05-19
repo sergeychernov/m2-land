a68d0313c4072201bd22a05e8e5d54e4fd025ca8
Victor Ivanov <victorivanov.spb@gmail.com>
2023-05-19 18:59:57 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,28 @@ import { Inter } from 'next/font/google'
 import { useTelegramInitData } from '../hooks/use-telegram-init-data';
 import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
+import styles from './styles.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} ${styles.bg}`}
     >
-      <Link href="/intro">правила игры</Link>
-      <Link href="/create-task/cms">создать задачу CMS</Link>
-      <Link href="/create-task/classified">создать задачу Classified</Link>
+      <Image
+          className={`relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] ${styles.logoImg}`}
+          src="/logo-large-gold.png"
+          alt="Alice M2 Logo"
+          width={232}
+          height={167}
+          priority
+      />
+      <Link href="/intro">Правила игры</Link>
+      <Link href="/create-task/cms">Создать задачу CMS</Link>
+      <Link href="/create-task/classified">Создать задачу Classified</Link>
       <Link href="/choose-task">Список доступных голосований</Link>
-
       <Link href="/voting/B4z8q6h4gtnDsAitKb9h">Проголосуйте за задачу B4z8q6h4gtnDsAitKb9h</Link>
-
     </main>
   )
 }
